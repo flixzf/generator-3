0a2b5e9c9ef6f8c4b42031e57ad25b4356f7fcda
flixzf <kdh@skorstudio.com>
2025-02-17 21:28:16 +0900

    Update next config for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     unoptimized: process.env.BUILD_TYPE === 'github' ? true : false,
   },
   basePath: process.env.BUILD_TYPE === 'github' ? '/generator-3' : '',  // 저장소 이름으로 수정
+  assetPrefix: process.env.BUILD_TYPE === 'github' ? '/generator-3' : '',  // 이 줄 추가
 };
 
 export default nextConfig;
